0a5d6668848fcc081c2db384d0832f75f06153e4
T618 <thienbao13102005@gmail.com>
2026-02-03 16:20:07 +0700

    Update  network socket, wifi và config

---
+++ include/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// WiFi - Thay "your_ssid" và "your_pass" bằng tên mạng và password của bạn
+static const char* WIFI_SSID = "Daily Coffee";     // Tên WiFi (SSID)
+static const char* WIFI_PASS = "666666666";     // Mật khẩu WiFi
