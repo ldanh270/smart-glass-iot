66e14e8db23880b0deefd528dfcaf5df7c4cadbb
bloan-7105 <taloan.8104@gmail.com>
2026-02-06 11:20:37 +0700

    feat: add drivers button

---
+++ src/drivers/button/button.cpp
@@ -0,0 +1,56 @@
+#include "button.h"
+
+Button::Button(uint8_t pin)
+  : pin(pin),
+    lastState(HIGH),
+    currentState(HIGH),
+    lastDebounceTime(0),
+    pressedTime(0),
+    holdTriggered(false) {}
+
+void Button::init() {
+  pinMode(pin, INPUT);
+}
+
+bool Button::isPressed() {
+  bool reading = digitalRead(pin);
+
+  if (reading != lastState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > DEBOUNCE_MS) {
+    if (reading != currentState) {
+      currentState = reading;
+
+      // Khi thả nút ra
+      if (currentState == HIGH && !holdTriggered) {
+        lastState = reading;
+        return true; // nhấn ngắn
+      }
+
+      // Khi nhấn nút xuống
+      if (currentState == LOW) {
+        pressedTime = millis();
+        holdTriggered = false;
+      }
+    }
+  }
+
+  lastState = reading;
+  return false;
+}
+
+bool Button::onHold() {
+  if (currentState == LOW && !holdTriggered) {
+    if (millis() - pressedTime >= HOLD_TIME_MS) {
+      holdTriggered = true;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool Button::isDownRaw() {
+  return digitalRead(pin) == LOW;
+}
--- src/drivers/button/button.h
+++ src/drivers/button/button.h
@@ -1,8 +1,45 @@
-#ifndef BUTTON_H // Header Guard
+#ifndef BUTTON_H
 #define BUTTON_H
 
-#include <Arduino.h> 
+#include <Arduino.h>
 
-// TODO: YOUR FUNCTION HERE
+/**
+ * `Button`: quản lý nút nhấn vật lý
+ * - init()        : khởi tạo button
+ * - isPressed()   : nhấn ngắn (click)
+ * - onHold()      : giữ lâu (hold)
+ */
+
+class Button {
+private:
+  uint8_t pin;
+
+  bool lastState;
+  bool currentState;
+
+  unsigned long lastDebounceTime;
+  unsigned long pressedTime;
+
+  bool holdTriggered;
+
+  static const unsigned long DEBOUNCE_MS = 50;
+  static const unsigned long HOLD_TIME_MS = 1000; // giữ > 1s
+
+public:
+  // Khởi tạo button với chân GPIO
+  Button(uint8_t pin);
+
+  // Setup pinMode
+  void init();
+
+  // Trả true khi nhấn ngắn
+  bool isPressed();
+
+  // Trả true khi giữ lâu
+  bool onHold();
+
+  // Trả true nếu chân button đang ở trạng thái LOW (nhấn) - đọc thô, không debounce
+  bool isDownRaw();
+};
 
 #endif
